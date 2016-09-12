1dc29a21be41d1d563de7a7aab1764d8d3d55fee
marcoschwartz <marcolivier.schwartz@gmail.com>
2016-09-12 22:34:46 +0200

    first commit

---
+++ module_3/01/raspberry_pi/public/js/script.js
@@ -0,0 +1,21 @@
+$( document ).ready(function() {
+
+  // Set pin to output
+  $.get('/lamp/mode/5/o');
+
+  // Buttons
+  $( "#on" ).click(function() {
+
+    // Set lamp ON
+    $.get('/lamp/digital/5/1');
+
+  });
+
+  $( "#off" ).click(function() {
+
+    // Set lamp OFF
+    $.get('/lamp/digital/5/0');
+
+  });
+
+});
